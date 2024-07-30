8a379415608b0965a98b81b97fcef81a20c733b2
John42506176Linux <johnkongtcheu@gmail.com>
2024-07-30 08:20:33 -0700

    Adding botocore

--- backend/deploy-task-service.sh
+++ backend/deploy-task-service.sh
@@ -11,11 +11,10 @@ REPOSITORY_NAME="sherlockholmie-report"
 ECR_URL="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/${REPOSITORY_NAME}:${IMAGE_VERSION}"
 
 # Build the image using docker-compose
-# docker-compose build report-task
+docker-compose build report-task
 
 # Tag the image
 docker tag ${IMAGE_NAME} ${ECR_URL}
-
 
 # Authenticate to ECR
 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
