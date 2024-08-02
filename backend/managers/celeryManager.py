63af402b05c87000e88ed5562beeb91bd947ff7b
John42506176Linux <johnkongtcheu@gmail.com>
2024-08-02 00:33:02 -0700

    Saving json

--- backend/managers/celeryManager.py
+++ backend/managers/celeryManager.py
@@ -5,6 +5,7 @@ from managers.databaseManager import DatabaseManager
 from managers.reportManager import ReportManager
 import os 
 import requests
+import json
 
 NEXTJS_API_URL=os.getenv('NEXTJS_API_URL')
 REDIS_URL = os.getenv('REDIS_URL', 'redis://redis:6379/0')
@@ -49,6 +50,8 @@ def process_space_task(params):
             "Pain Points": pain_points_dict,
             "Personas": personas_dict
         }
+        with open(f'result-{params["request_id"]}.json', 'w') as f:
+            json.dump(result, f)
 
         # Send result to Next.js API
         requests.post(NEXTJS_API_URL, json={"requestId": params['request_id'], "result": result})
